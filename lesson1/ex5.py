942c9e7a92fc29af6a7c61a25b617fb7cf0fea26
Pola Madej <Pola.Madej@motorolasolutions.com>
2023-10-14 13:47:02 +0200

    lesson1

---
+++ lesson1/ex5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# hello.py
+#
+# Pierwszy program w Pythonie.
+# W pierwszej linii mamy ścieżkę dostępu do interpretera.
+# W drugiej linii informacja o kodowaniu znaków w pliku.
+# Dzięki temu można w pliku stosować polskie znaki w napisach.
+# Po znaku '#' mamy komentarz do końca linii.
+
+print ( "Hello, world!" )   # działa dla Pythona 2 i 3
+# Ustawienie kodowania UTF-8.
+# -*- coding: utf-8 -*-   # styl edytora Emacs
+# coding: utf-8         # to wystarczy
+# coding=utf-8          # to wystarczy
+# Ustawienie kodowania ISO dla języka polskiego.
+# -*- coding: iso-8859-2 -*-
+# Ustawienie ścieżki do Pythona w środowisku wirtualnym i nie tylko.
+#!/usr/bin/python       # NIE UWZGLĘDNIA ŚRODOWISKA!
+#!/usr/bin/env python
+#!/usr/bin/env python2
+#!/usr/bin/env python3
