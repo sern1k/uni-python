f6f5e1c998c1d4481947a3c352acb261ec286027
Pola Madej <Pola.Madej@motorolasolutions.com>
2023-10-20 12:26:44 +0200

    quick fixes

--- lesson2/ex18/ex18.py
+++ lesson2/ex18/ex18.py
@@ -1,9 +1,7 @@
 def count_zeros_in_large_number(number):
     number_str = str(number)
-
     count = number_str.count("0")
-
     return count
 
-large_number = 102030405060700
+large_number = 10203040506070
 print(f"The number of zeros in the number {large_number} is: {count_zeros_in_large_number(large_number)}")
