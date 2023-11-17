09796528d5da0fef666e7e458a84e360ff0887fb
Pola Madej <Pola.Madej@motorolasolutions.com>
2023-11-17 22:28:02 +0100

    initialization the project

---
+++ project/main.py
@@ -0,0 +1,18 @@
+class ConnectFourGame:
+  def __init__(self):
+    pass
+
+  def print_board(self):
+    pass
+
+  def check_winner(self):
+    pass
+
+  def player_move(self):
+    pass
+
+  def computer_move(self):
+    pass
+
+  def play(self):
+    pass
